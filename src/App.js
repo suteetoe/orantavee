2e2b7fe5082628a559a76754ccd5764a6a1ccf28
suteetoe <sutee.toe@gmail.com>
2018-08-16 19:28:06 +0700

    add template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navigator from './components/Navigator';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navigator />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <nav>
-          NAV BAR
-        </nav>
+        
         <div class="container" >        
           <h2>MAIN Container</h2>
             <p className="App-intro">
